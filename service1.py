1f3dbb57f4fdac3f0930028175af6d8035f2075b
EricYang <ericyang@hyenatek.com>
2016-07-25 17:07:33 +0800

    add service1.py

---
+++ service1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint
+from flask import render_template
+
+# 定義一個blueprint,其實也就是一個模塊
+btest = Blueprint("service1", __name__)
+
+
+@btest.route("/")
+def test_model():
+    return render_template('en/index.html')
